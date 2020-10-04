fef83bc733f7c2b8ae6ec51106f77f87b0857a2a
YOSHIOKA Takuma <lo48576@hard-wi.red>
2020-10-05 00:21:36 +0900

    Implement `Ymd8HyphenStr::days_since_epoch` method

--- src/common/ymd8_hyphen.rs
+++ src/common/ymd8_hyphen.rs
@@ -895,6 +895,44 @@ impl Ymd8HyphenStr {
             non_leap_yday
         }
     }
+
+    /// Returns the days from the epoch (1970-01-01).
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use datetime_string::common::Ymd8HyphenStr;
+    /// let date = Ymd8HyphenStr::from_str("1971-01-01")?;
+    /// assert_eq!(date.days_since_epoch(), 365);
+    ///
+    /// let date2 = Ymd8HyphenStr::from_str("1969-01-01")?;
+    /// assert_eq!(date2.days_since_epoch(), -365);
+    ///
+    /// let epoch = Ymd8HyphenStr::from_str("1970-01-01")?;
+    /// assert_eq!(epoch.days_since_epoch(), 0);
+    /// # Ok::<_, datetime_string::Error>(())
+    /// ```
+    pub fn days_since_epoch(&self) -> i32 {
+        let tm_year = i32::from(self.year()) - 1900;
+
+        // See "4.16. Seconds Since the Epoch" section of POSIX
+        // (<https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap04.html#tag_04_16>).
+        //
+        // > If the year is \<1970 or the value is negative, the relationship is
+        // > undefined. If the year is >=1970 and the value is non-negative, the
+        // > value is related to a Coordinated Universal Time name according to
+        // > the C-language expression, where `tm_sec`, `tm_min`, `tm_hour`,
+        // > `tm_yday`, and `tm_year` are all integer types:
+        // >
+        // > ```
+        // > tm_sec + tm_min*60 + tm_hour*3600 + tm_yday*86400 +
+        // >     (tm_year-70)*31536000 + ((tm_year-69)/4)*86400 -
+        // >     ((tm_year-1)/100)*86400 + ((tm_year+299)/400)*86400
+        // > ```
+        (i32::from(self.yday1()) - 1) + (tm_year - 70) * 365 + (tm_year - 69) / 4
+            - (tm_year - 1) / 100
+            + (tm_year + 299) / 400
+    }
 }
 
 #[cfg(feature = "alloc")]
